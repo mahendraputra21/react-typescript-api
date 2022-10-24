0ded9d74d04fbc830757743e668d7e808445c863
Dewa Mahendra <dewa.mahendra@geekseat.com.au>
2022-10-24 14:14:32 +0800

    fix redirect  to products

--- src/components/Tutorial.tsx
+++ src/components/Tutorial.tsx
@@ -5,7 +5,7 @@ import TutorialDataService from "../services/TutorialService";
 import ITutorialData from "../types/Tutorial";
 
 const Tutorial: React.FC = () => {
-  const { id }= useParams();
+  const { id } = useParams();
   let navigate = useNavigate();
 
   const initialTutorialState = {
@@ -54,7 +54,7 @@ const Tutorial: React.FC = () => {
     TutorialDataService.remove(currentTutorial.id)
       .then((response: any) => {
         console.log(response.data);
-        navigate("/tutorials");
+        navigate("/products");
       })
       .catch((e: Error) => {
         console.log(e);
@@ -63,65 +63,65 @@ const Tutorial: React.FC = () => {
 
   return (
     <div>
-      {currentTutorial ? 
-      (
-        <div className="edit-form">
-          <h4>Products</h4>
-          <form>
-            <div className="form-group">
-              <label htmlFor="name">Name</label>
-              <input
-                type="text"
-                className="form-control"
-                id="name"
-                name="name"
-                value={currentTutorial.name}
-                onChange={handleInputChange}
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="description">Description</label>
-              <input
-                type="text"
-                className="form-control"
-                id="description"
-                name="description"
-                value={currentTutorial.description}
-                onChange={handleInputChange}
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="price">Price</label>
-              <input
-                type="text"
-                className="form-control"
-                id="price"
-                name="price"
-                value={currentTutorial.price}
-                onChange={handleInputChange}
-              />
-            </div>
-          </form>
+      {currentTutorial ?
+        (
+          <div className="edit-form">
+            <h4>Products</h4>
+            <form>
+              <div className="form-group">
+                <label htmlFor="name">Name</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="name"
+                  name="name"
+                  value={currentTutorial.name}
+                  onChange={handleInputChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="description">Description</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="description"
+                  name="description"
+                  value={currentTutorial.description}
+                  onChange={handleInputChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="price">Price</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="price"
+                  name="price"
+                  value={currentTutorial.price}
+                  onChange={handleInputChange}
+                />
+              </div>
+            </form>
 
-          <button className="badge badge-danger mr-2" onClick={deleteTutorial}>
-            Delete
-          </button>
+            <button className="badge badge-danger mr-2" onClick={deleteTutorial}>
+              Delete
+            </button>
 
-          <button
-            type="submit"
-            className="badge badge-success"
-            onClick={updateTutorial}
-          >
-            Update
-          </button>
-          <p>{message}</p>
-        </div>
-      ) : (
-        <div>
-          <br />
-          <p>Please click on a Product Name...</p>
-        </div>
-      )}
+            <button
+              type="submit"
+              className="badge badge-success"
+              onClick={updateTutorial}
+            >
+              Update
+            </button>
+            <p>{message}</p>
+          </div>
+        ) : (
+          <div>
+            <br />
+            <p>Please click on a Product Name...</p>
+          </div>
+        )}
     </div>
   );
 };
